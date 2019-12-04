324068484c9572bf0e25615397a1f073597fcc9f
liad <liadna@post.bgu.ac.il>
2019-12-04 21:26:31 +0200

    lambda + applic

--- tag-parser.ml
+++ tag-parser.ml
@@ -1,5 +1,7 @@
 #use "reader.ml";;
-
+open Reader;;
+(*#use "tag-parser.ml";;
+open Tag_Parser;;*)
 type constant =
   | Sexpr of sexpr
   | Void
@@ -43,13 +45,16 @@ let rec expr_eq e1 e2 =
 	
                        
 exception X_syntax_error;;
-
+exception X_excp;;
+(*
 module type TAG_PARSER = sig
   val tag_parse_expression : sexpr -> expr
   val tag_parse_expressions : sexpr list -> expr list
 end;; (* signature TAG_PARSER *)
 
 module Tag_Parser : TAG_PARSER = struct
+*)
+module Tag_Parser = struct 
 
 let reserved_word_list =
   ["and"; "begin"; "cond"; "define"; "else";
@@ -58,55 +63,71 @@ let reserved_word_list =
    "unquote-splicing"];;  
 
 (* work on the tag parser starts here *)
-let isReserved str = List.fold_right (fun curr acc-> if(curr == str) then true else acc) reserved_word_list  false;;
+let isReserved str = List.fold_right (fun curr acc-> if(curr = str) then true else acc) reserved_word_list false;;
 (*pair to proper list*)
 let rec pair_to_list f pair= match pair with
-|Pair(x, Nil) -> []
+|Pair(x, Nil) -> [f x]
 |Pair(x, rest) -> (f x) :: (pair_to_list f rest)
 |_ -> raise X_syntax_error;;
 
-let rec isProper pair = match pair with
-|Pair(x, Nil) -> true
-|Pair(x, y) -> isProper(y)
-|Pair(x,_) -> false (*improper list*)
-|_-> raise X_syntax_error;; 
+let rec getOptinal pair = match pair with
+|Pair(x, Nil) -> ""
+|Pair(x, y) -> 
+match y with
+  |Pair(_,_) -> getOptinal(y)
+  |Symbol(sym) -> sym 
+  |_ -> raise X_syntax_error
+|_-> raise X_syntax_error;;
 
 let pull_string pair = pair_to_list 
 (fun(x)-> match x with
 |Symbol(x)-> x
 |_ -> raise X_syntax_error) pair;;
 
-let tag_parse_expression sexpr = function  
-(*| Pair(Symbol("let"), Pair(Nil, Pair(body, Nil))) ->
-| Pair(Symbol("let"), Pair(Pair(rib, ribs), Pair(body, Nil))) -> *)
+let rec tag_pars sexpr = match sexpr with
+| Pair(Symbol("lambda"), Pair(list_of_param, Pair(body, Nil)))-> pull_string list_of_param
+|_-> raise X_syntax_error;;
 
+let rec tag_parse sexpr = match sexpr with 
+(*| Pair(Symbol("let"), Pair(Nil, Pair(body, Nil))) -> 
+| Pair(Symbol("let"), Pair(Pair(rib, ribs), Pair(body, Nil))) ->  *)
 (*to check*)
-(*| Pair(closure, args_list)-> Applic((tag_parse_expression closure), (pair_to_list tag_parse_expression args_list))
-
+| Pair(closure, args_list)-> Applic((tag_parse closure), (pair_to_list tag_parse args_list))
+| Pair(Symbol("lambda"), Pair(Symbol(sym), Pair(body, Nil)))-> LambdaOpt([],sym, tag_parse body)
 | Pair(Symbol("lambda"), Pair(list_of_param, Pair(body, Nil)))-> 
-  if(isProper list_of_param) then LambdaSimple(pull_string list_of_param , tag_parse_expression body)
-  else LambdaOpt(list_of_param, optional, tag_parse_expression body)
-| Pair(Symbol("lambda"), Pair(Symbol(sym), Pair(body, Nil)))-> LambdaOpt([],sym, tag_parse_expression body)
-*)
+  let optional = getOptinal list_of_param in 
+  if(optional = "") then LambdaSimple(pull_string list_of_param , tag_parse body)
+  else LambdaOpt(pull_string list_of_param, optional, tag_parse body)
 (*to check*)
-(*| Pair(Symbol("if"), Pair(test, Pair(dit, Pair(dif, Nil)))) ->
-If(tag_parse_expression  test, tag_parse_expression dit, tag_parse_expression dif)
+| Pair(Symbol("if"), Pair(test, Pair(dit, Pair(dif, Nil)))) ->
+ If(tag_parse  test, tag_parse dit, tag_parse dif)
 | Pair(Symbol("if"), Pair(test,Pair(dit, Nil))) ->
-If(tag_parse_expression  test, tag_parse_expression dit, Const(Void))
+ If(tag_parse  test, tag_parse dit, Const(Void))
 | Pair(Symbol("quote"), Pair(x, Nil)) -> Const(Sexpr(x)) 
-| Symbol(x) -> if(isReserved(x) = false) then Var(Sexpr(Symbol(x))) 
+| Symbol(x) ->  if(isReserved(x) = false) then let () = printf("here") in Var(x) else raise X_syntax_error
 | TagRef(x) -> Const(Sexpr(TagRef(x)))
 | TaggedSexpr (st, Pair (Symbol "quote", Pair (x, Nil))) -> Const(Sexpr(TaggedSexpr(st, x)))
 | TaggedSexpr (st,x) -> Const(Sexpr(TaggedSexpr(st, x)))
-*)
+
 (*unquoted sexspr *)
 | Number(x) -> Const(Sexpr(Number(x)))
 | Bool(x) -> Const(Sexpr(Bool(x)))
 | Char(x) -> Const(Sexpr(Char(x)))
 | String(x) -> Const(Sexpr(String(x)))
+|_-> raise X_syntax_error;;
+
+let tag_parse_expression sexpr = tag_parse sexpr;;
 
 
 let tag_parse_expressions sexpr = raise X_not_yet_implemented;;
 
   
 end;; (* struct Tag_Parser *)
+(*
+Pair (Symbol "lambda",
+ Pair
+  (Pair (Symbol "x",
+    Pair (Pair (Symbol "unquote", Pair (Symbol "y", Nil)), Nil)),
+  Pair (Pair (Symbol "+", Pair (Symbol "x", Pair (Symbol "y", Nil))), Nil)))*)
+
+ 
