def7bc9e5bc73a108277be43a7ad92af68ff6a1a
liad <liadna@post.bgu.ac.il>
2020-01-06 23:16:32 +0200

    1

--- code-gen.ml
+++ code-gen.ml
@@ -45,7 +45,7 @@ module Code_Gen = struct
   | _ -> [];;
 
   let rec extend_consts_table const = match const with
-    | Sexpr(Symbol(string)) -> [Sexpr(String(string))]
+    | Sexpr(Symbol(string)) -> [Sexpr(String(string)); const]
     | Sexpr(Pair(car_const, cdr_const)) -> (extend_consts_table (Sexpr(car_const))) @  (extend_consts_table (Sexpr(cdr_const))) @ [const]
     | Sexpr(TaggedSexpr(string, sexpr)) -> extend_consts_table (Sexpr(sexpr))
     |_-> [const];;
@@ -60,6 +60,43 @@ module Code_Gen = struct
   let reduce_list l = 
     List.fold_right (fun curr acc -> if(exists curr acc) then acc else curr::acc) l [];;
 
+  let get_const_address table const = 
+  List.fold_left (fun acc (curr_const, (offset, string))-> match curr_const, const with 
+  |Void , Void ->  offset 
+  |Sexpr(sexp1), Sexpr(sexp2) -> if (sexpr_eq sexp1 sexp2) then offset else acc
+  |_,_-> acc) (-1) table;; (*maybe throw an error???*)
+  
+   let get_const_address_2 table const = 
+  List.fold_left (fun acc (curr_const, (offset, string))-> match curr_const, const with 
+  |Void , Void ->  offset 
+  |Sexp(TagRef(name1)), Sexp(TagRef(name2)) -> if(name1 = name2) then (get_const_address map.get(name1))
+  |Sexpr(sexp1), Sexpr(sexp2) -> if (sexpr_eq sexp1 sexp2) then offset else acc
+  |_,_-> acc) (-1) table;; (*maybe throw an error???*)
+
+  (*returns entry and the size of the const*)
+  let build_const_entry const table offset = match const with 
+  |Void -> ( (const, (offset, "MAKE_VOID")) , 1)
+  |Sexpr(Nil) -> ( (const, (offset, "MAKE_NIL")), 1)
+  |Sexpr(Bool false) -> ( (const, (offset, "MAKE_BOOL(0)")), 2)
+  |Sexpr(Bool true) -> ( (const, (offset, "MAKE_BOOL(1)")), 2)
+  |Sexpr(Number(Int(num))) -> ( (const, (offset, "MAKE_LITERAL_INT(" ^ (string_of_int num) ^")")), 9)
+  |Sexpr(Number(Float(num))) -> ( (const, (offset, "MAKE_LITERAL_FLOAT(" ^ (string_of_float num) ^")")), 9)
+  |Sexpr(Char(c)) -> ( (const, (offset, "MAKE_LITERAL_CHAR("^ (Char.escaped c) ^")")), 2)
+  |Sexpr(String(str)) -> ( (const, (offset, " MAKE_LITERAL_STRING(\" "^ str ^" \")")), (String.length str) + 9) (*to check!*)
+  |Sexpr(Symbol(str)) -> ( (const, (offset, " MAKE_LITERAL_SYMBOL(consts+ "^ (string_of_int (get_const_address table (Sexpr(String(str))))) ^")")), 9)
+  |Sexpr(Pair(sexpr1, sexpr2)) -> ( (const, (offset, " MAKE_LITERAL_PAIR(consts+ "^ (string_of_int (get_const_address table (Sexpr(sexpr1)))) ^"
+  , consts+" ^ (string_of_int (get_const_address table (Sexpr(sexpr2)))) ^")")), 17)
+  |Sexpr(TagRef(name)) -> let exp = map.get(name) build_const_entry(exp)
+  
+
+  let build_consts_table consts_list = 
+  let (table, last_offset) = 
+  List.fold_left (fun (table, offset) curr -> 
+  let (const_entry, size_of_const) = build_const_entry curr table offset in
+  (table@[const_entry], offset+size_of_const ))  ([], 0) consts_list
+  in table;;
+  
+
   let check string = 
   let string_to_asts s = List.map Semantics.run_semantics
                          (Tag_Parser.tag_parse_expressions
@@ -69,18 +106,16 @@ module Code_Gen = struct
   let must_const = [Void ;Sexpr(Nil); Sexpr(Bool false) ; Sexpr(Bool true)] in
   let list_of_consts = List.flatten (List.map make_list_of_all_consts asts) in
   let extended_list = must_const @ List.flatten(List.map extend_consts_table list_of_consts) in
-  let reduce_list = reduce_list extended_list in
-  reduce_list;;
-
-  let build_consts_table l =
-   raise X_not_yet_implemented;;
+  let () = biuld_table expand_list in
+  let reduced = reduce_list extended_list in
+  let table_1 = build_consts_table reduced in build_consts_table reduced table
 
   let make_consts_tbl asts = 
   let must_const = [Void ;Sexpr(Nil); Sexpr(Bool false) ; Sexpr(Bool true)] in
   let list_of_consts = List.flatten (List.map make_list_of_all_consts asts) in
   let extended_list = must_const @ List.flatten (List.map extend_consts_table list_of_consts) in
-  let reduce_list = reduce_list extended_list in
-  build_consts_table reduce_list;;
+  let reduced = reduce_list extended_list in
+  build_consts_table reduced;;
 
 let exists_str curr_const consts_list = 
   List.fold_left (fun acc curr -> if (curr = curr_const) then true else acc) false consts_list;;
